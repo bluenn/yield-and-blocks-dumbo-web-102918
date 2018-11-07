0ff75ef1846266f73a5a30c660c04bb97e9d86fa
a lan <bluenowte97@gmail.com>
2018-11-07 00:33:18 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,7 @@
 def hello_t(array)
+
   if block_given?
   i = 0
-
   while i < array.length
     yield(array[i])
     i = i + 1
@@ -11,7 +11,6 @@ def hello_t(array)
  else
    puts "Hey! No block was given!"
  end
-
 end
 # call your method here!
 
